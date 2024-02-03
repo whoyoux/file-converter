4945f278aa8da714ee3083c13789ff5e28ae40d4
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-02-03 01:01:44 +0100

    added cta button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
 import Link from "next/link";
 import { ThemeDropdown } from "./theme-dropdown";
 
 const Header = () => {
 	return (
 		<header className="w-full min-w-0 px-4 md:px-8 xl:px-12 border-b flex items-center justify-between">
-			<h1 className="py-4 md:py-6 text-2xl font-semibold">File converter</h1>
-			<div className="flex items-center gap-2 md:gap-6">
+			<Link href="/">
+				<h1 className="py-4 md:py-6 text-2xl font-semibold">File converter</h1>
+			</Link>
+			<div className="flex items-center gap-2 md:gap-4">
+				<Link href="/convert">
+					<Button>Convert now</Button>
+				</Link>
+				<ThemeDropdown />
 				<Link
 					href="https://github.com/whoyoux/file-converter"
 					target="_blank"
 					referrerPolicy="no-referrer"
 				>
-					<Button variant="link">source code</Button>
+					<Button variant="outline" size="icon">
+						<Github className="h-[1.2rem] w-[1.2rem]" />
+					</Button>
 				</Link>
-				<ThemeDropdown />
 			</div>
 		</header>
 	);
